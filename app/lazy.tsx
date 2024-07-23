3e4def6569298ace8cd78c796ece8c66331362c7
goncy <gonzalo.pozzo4@gmail.com>
2024-07-23 09:16:09 -0300

    Just call it as a component

--- app/lazy.tsx
+++ app/lazy.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { use, unstable_postpone as postpone } from "react";
+import { unstable_postpone as postpone } from "react";
 
 export default function Lazy<P>({as: Component, ...props}: P & {as: (props: P) => Promise<React.ReactElement>}) {
   if (typeof window === "undefined") postpone("Omit SSR")
   
-  return use(Component(props as P))
+  return <Component {...props as P} />
 }
