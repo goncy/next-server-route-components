456b5e2815058f63a34b77d2e14a1e4705b471c1
goncy <gonzalo.pozzo4@gmail.com>
2024-07-23 10:01:45 -0300

    Use transition

--- app/lazy.tsx
+++ app/lazy.tsx
@@ -2,6 +2,12 @@
 
 import { unstable_postpone as postpone, useEffect, useState, useTransition } from "react";
 
+/**
+ * ## Current iteration of the idea
+ *
+ * We still fetch only once but we get the suspense behavior we want by using postpone during pre-rendering and a transition during client side fetching.
+ */
+
 export default function Lazy<P>({as: getPayload, ...props}: P & {as: (props: P) => Promise<React.ReactElement>}) {
   const [payload, setPayload] = useState<React.ReactElement>(null)
   const [isLoading, startTransition] = useTransition()
@@ -17,45 +23,42 @@ export default function Lazy<P>({as: getPayload, ...props}: P & {as: (props: P) 
   return payload
 }
 
-/*
-Previous iterations of this idea:
-
-## Using `useEffect`
-
-```jsx
-  const [payload, setPayload] = useState<React.ReactElement>(null)
-  const [isLoading, startTransition] = useTransition()
-
-  useEffect(() => {
-    startTransition(async () => {
-      setPayload(await getPayload(props as P))
-    })
-  }, [])
-
-  if (typeof window === "undefined" || isLoading) postpone("Omit SSR")
-  
-  return payload
-```
-
-> This is the "safer" approach as you get sure you only do the fetching once
-
-## Using server action as a component
-
-```jsx
-  if (typeof window === "undefined") postpone("Omit SSR")
-  
-  return <Component {...props as P} />
-```
-
-> The tradeoff here is that this server action is called in every render, afaik server actions are not cached by default so that's a big issue.
-
-## Using `use`
-
-```jsx
-  if (typeof window === "undefined") postpone("Omit SSR")
-  
-  return use(Component(props as P))
-```
-
-> The tradeoff here is that we don't use a component (a server action as a component) when we could. Not sure if `use` has some kind of caching that might help for this case.
-*/
+/**
+ * ## Previous iterations of this idea:
+ * 
+ * ### Using `useEffect`
+ * 
+ * ```jsx
+ * const [payload, setPayload] = useState<React.ReactElement>(null)
+ * 
+ * useEffect(() => {
+ *   getPayload(props as P).then(setPayload)
+ * }, [])
+ * 
+ * if (typeof window === "undefined") postpone("Omit SSR")
+ * 
+ * return payload
+ * ```
+ * 
+ * > This is the "safer" approach as you ensure that the fetching is done only once. However, I couldn't find a way to postpone the component while `useEffect` is running, so the component is only postponed during pre-render.
+ * 
+ * ### Using server action as a component
+ * 
+ * ```jsx
+ * if (typeof window === "undefined") postpone("Omit SSR")
+ * 
+ * return <Component {...props as P} />
+ * ```
+ * 
+ * > The tradeoff here is that this server action is called in every render, and server actions are not cached by default, which can be a performance issue.
+ * 
+ * ## Using `use`
+ * 
+ * ```jsx
+ * if (typeof window === "undefined") postpone("Omit SSR")
+ * 
+ * return use(Component(props as P))
+ * ```
+ * 
+ * > The tradeoff here is that we don't use a component (a server action as a component) when we could. I'm not sure if `use` has some kind of caching that might help in this case.
+ */
