1fcd962a68a231da4409f5c82d083c954f07d547
goncy <gonzalo.pozzo4@gmail.com>
2024-07-23 09:27:39 -0300

    Add disclaimers

--- app/lazy.tsx
+++ app/lazy.tsx
@@ -7,3 +7,27 @@ export default function Lazy<P>({as: Component, ...props}: P & {as: (props: P) =
   
   return <Component {...props as P} />
 }
+
+/*
+Previous iterations of this idea:
+
+## Using server action as a component
+
+  ```jsx
+  if (typeof window === "undefined") postpone("Omit SSR")
+  
+  return <Component {...props as P} />
+  ```
+
+> The tradeoff here is that this server action is called in every render, afaik server actions are not cached by default so that's a big issue.
+
+## Using `use`
+
+  ```jsx
+  if (typeof window === "undefined") postpone("Omit SSR")
+  
+  return use(Component(props as P))
+  ```
+
+> The tradeoff here is that we don't use a component (a server action as a component) when we could. Not sure if `use` has some kind of caching that might help for this case.
+*/
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: {
     ppr: true
+  },
+  typescript: {
+    ignoreBuildErrors: true // Returning a Promise as JSX
   }
 };
 
